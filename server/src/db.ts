da8d3647b74f6f62ff9d2d5ee77aaadbb8ac518b
diegoss-github <diegoss@bu.edu>
2024-01-20 08:28:43 +0000

    process.env type error fixed

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_URL: String = process.env.MONGO_URL;
+const MONGO_URL: string | undefined = process.env.MONGO_URL;
 
 mongoose.connect(`${MONGO_URL}`).then(() => {
   console.log("DB connected 🪩");
