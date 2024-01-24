4c6a8943e093c1db29ec95442d9412b162c606c7
diegoss-github <diegoss@bu.edu>
2024-01-24 19:07:24 +0000

    tests for SearchResults

--- server/dist/db.js
+++
@@ -1,6 +0,0 @@
-const MONGO_URL = process.env.MONGO_URL;
-const mongoose = require("mongoose");
-mongoose.connect(`${MONGO_URL}`).then(() => {
-    console.log("DB connected 🪩");
-});
-module.exports = mongoose;
--- server/dist/router.js
+++
@@ -1,6 +0,0 @@
-const server = require("express");
-const serverRouter = server.Router();
-const controller = require("./controller");
-serverRouter.post("/savePlaylist", controller.savePlaylist);
-serverRouter.get("/getCollections", controller.getCollections);
-module.exports = serverRouter;
